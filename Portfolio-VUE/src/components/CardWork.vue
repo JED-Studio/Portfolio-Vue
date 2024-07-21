747ea1cb8271972f5684cd660e5e110eedd654e3
Ramazan <101656313+JED-Studio@users.noreply.github.com>
2024-07-21 04:49:38 +0300

    Work_Card

---
+++ Portfolio-VUE/src/components/CardWork.vue
@@ -0,0 +1,32 @@
+
+
+<script setup>
+
+const props = defineProps({
+  title: String,
+  description: String,
+  imageUrl: String,
+  stack_list_title: String,
+  stack_list: String,
+  links: String
+ 
+})
+
+</script>
+
+<template>
+     <div class="item flex flex-col rounded-lg" style="box-shadow: 0 3px 6px rgba(0, 0, 0, .16), 0 3px 6px rgba(0, 0, 0, .23); background-color: #202020; overflow: hidden;">
+          <img src="{{ imageUrl }}" alt="">
+          <div class="p-6">
+          <h3 class="mb-4 font-black text-2xl leading-tight">{{ title }}</h3>
+          <p>{{ description }}
+          </p>
+          <div class="my-5" style="background-color: #7f5af0; padding: 1px;"></div>
+          <h4 class="mb-4 font-black text-2xl leading-tight">{{ stack_list_title }}</h4>
+            <ul type="square">
+              <li>{{ stack_list }}</li>
+              <li>{{ links }}</li>
+            </ul>
+          </div>
+        </div>
+</template>
---
+++ Portfolio-VUE/src/components/SectionWork.vue
@@ -0,0 +1,97 @@
+
+<script setup>
+
+import CardWork from './CardWork.vue';
+defineProps({
+    workWeb:Array,
+
+})
+
+const workWEB = [
+  {
+    "id":1,
+    "title": "Одностраничный сайт",
+    "description": "Разработка одностраничного сайта с использованием HTML, CSS и JavaScript.",
+    "stack_list_title": "Стек технологий:",
+    "stack_list": "HTML5, CSS3, JavaScript, VUE",
+    "links": "",
+    "imageUrl": "https://cdn-icons-png.flaticon.com/512/149/149071.png",
+  },
+
+ 
+]
+</script>
+
+
+
+
+
+
+<template>
+  
+<div class="section_work">
+  <div class="container max-w-7xl mx-auto pl-4 pr-4">
+    <div class="py-4 text-white">
+      <h2 style="text-align: center; margin-bottom: 32px; font-size: clamp(2rem,4vw,3rem); font-weight: 900;">{{$t('itemJob')}}</h2>
+<div class="flex justify-center gap-4">
+  
+      <button class="tabs_btn cursor-pointer flex justify-center items-center gap-1 rounded"  style="font-size: 16px; border: 1px solid #7f5af0; padding: 12px 16px; transition: background-color .2s linear, fill .2s linear;" data-type="layout">
+        <span data-localization-key="layout">Вёрстка</span>
+         <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" fill="#6c63ff" viewBox="0 0 502.664 502.664" style="enable-background:new 0 0 502.664 502.664"  xml:space="preserve"  ><g><g>
+          <path style="transition: background-color .2s linear, fill .2s linear;" fill="#6c63ff;" d="M153.821,358.226L0,274.337v-46.463l153.821-83.414v54.574L46.636,250.523l107.185,53.431
+			C153.821,303.954,153.821,358.226,153.821,358.226z">
+    </path>
+    <path style="transition: background-color .2s linear, fill .2s linear;" fill="#6c63ff;" d="M180.094,387.584L282.103,115.08h32.227L212.084,387.584H180.094z"></path>
+    <path style="transition: background-color .2s linear, fill .2s linear;" fill="#6c63ff;" d="M348.843,358.226v-54.272l107.164-52.999l-107.164-52.59v-53.927l153.821,83.522v46.183
+			L348.843,358.226z">
+    </path></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g>
+  </svg>
+</button>
+
+
+      <button class="tabs_btn tabs_btn--active cursor-pointer flex justify-center items-center gap-1 rounded"  style="font-size: 16px; border: 1px solid #7f5af0; padding: 12px 16px; transition: background-color .2s linear, color .2s linear;" data-type="projects">
+        <span>VUE / JS</span> 
+      
+<svg fill="#6c63ff" width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg" style="transition: background-color .2s linear, fill .2s linear;"><title>Vue.js icon</title><path d="M24,1.61H14.06L12,5.16,9.94,1.61H0L12,22.39ZM12,14.08,5.16,2.23H9.59L12,6.41l2.41-4.18h4.43Z"/>
+</svg>
+      </button>
+</div>
+
+      <div class="flex gap-4">
+        <CardWork
+        v-for="item in workWEB"
+        :key="item.id"
+        :item="item"
+        
+        />
+        
+
+
+        
+      </div>
+    </div>
+
+  </div>        
+
+</div>
+
+</template>
+
+<style scoped>
+
+
+.tabs_btn:hover{
+  background-color: #7f5af0;
+}
+
+.tabs_btn:hover svg{
+
+  fill: white;
+}
+
+li{
+  
+}
+
+
+</style>
