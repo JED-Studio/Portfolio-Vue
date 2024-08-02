c8b21a5b4b43dccd69f6d42d9f92778f9e22b8cb
Ramazan <101656313+JED-Studio@users.noreply.github.com>
2024-08-02 16:32:29 +0300

    Vite

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicPath: '/portfolio-vue/',
+  base: '/portfolio-vue/',
   plugins: [
     vue(),
     vueJsx(),
