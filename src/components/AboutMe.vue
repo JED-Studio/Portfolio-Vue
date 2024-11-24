ec73a824acab08cc726e3c4b9de26b94f7b21b08
Ramazan <romagt2015@mail.ru>
2024-11-24 23:16:05 +0300

    AboutMe
    Оптимизация

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,17 +1,17 @@
 <template>
-<div class="section_aboutMe">
+<div class="portfolio-vue__section-aboutMe">
   <div class="container  mx-auto pl-4 pr-4" style="max-width: 1400px;">
     <div class="py-4">
       <div class="" style="color: var(--background-color-luna);">
-        <h2 style="text-align: center; margin-bottom: 32px; font-size: clamp(2rem,4vw,3rem); font-weight: 900;">{{$t('aboutMe')}}</h2>
+        <h2 class="portfolio-vue__aboutMe">{{$t('aboutMe')}}</h2>
         <div class="flex flex-col gap-5">
-        <p style="font-size: clamp(1.125rem,2vw,1.25rem);">🧑🏽‍💻
+        <p class="portfolio-vue__aboutMe-p" >🧑🏽‍💻
           {{$t('aboutMe1')}}
         </p>
-        <p style="font-size: clamp(1.125rem,2vw,1.25rem);">🖥️
+        <p class="portfolio-vue__aboutMe-p">🖥️
           {{$t('aboutMe2')}}
         </p>
-        <p style="font-size: clamp(1.125rem,2vw,1.25rem);">📚
+        <p class="portfolio-vue__aboutMe-p">📚
           {{$t('aboutMe3')}}
         </p>
       </div>
@@ -23,4 +23,17 @@
 
 </div>
 </template>
---background-color-luna
+
+<style scoped>
+.portfolio-vue__aboutMe{
+  text-align: center; 
+  margin-bottom: 32px; 
+  font-size: clamp(2rem,4vw,3rem); 
+  font-weight: 900;
+}
+
+.portfolio-vue__aboutMe-p{
+  font-size: clamp(1.125rem,2vw,1.25rem);
+}
+</style>
+
